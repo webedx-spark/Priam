24d9764f4bbafc55938a6efc1803057c7f289401
Daniel Chia <danchia@coursera.org>
2014-06-09 14:55:11 -0700

    use IAMCreds

--- priam/src/main/java/com/netflix/priam/defaultimpl/PriamGuiceModule.java
+++ priam/src/main/java/com/netflix/priam/defaultimpl/PriamGuiceModule.java
@@ -24,6 +24,8 @@ import com.netflix.priam.aws.S3FileSystem;
 import com.netflix.priam.backup.IBackupFileSystem;
 
 import com.netflix.priam.ICredential;
+import com.netflix.priam.aws.IAMCredential;
+
 
 
 public class PriamGuiceModule extends AbstractModule
@@ -36,6 +38,6 @@ public class PriamGuiceModule extends AbstractModule
         bind(IBackupFileSystem.class).annotatedWith(Names.named("backup")).to(S3FileSystem.class);
         bind(IBackupFileSystem.class).annotatedWith(Names.named("incr_restore")).to(S3FileSystem.class);
         bind(IBackupFileSystem.class).annotatedWith(Names.named("backup_status")).to(S3FileSystem.class);
-        bind(ICredential.class).to(ClearCredential.class);
+        bind(ICredential.class).to(IAMCredential.class);
     }
 }
